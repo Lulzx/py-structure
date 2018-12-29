78db8daeed949f69067babf3ef57cac46263be60
lulzx <22330483+Lulzx@users.noreply.github.com>
2018-12-29 12:41:48 +0530

    shiny new things

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-import re
 import os
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
@@ -26,13 +25,14 @@ def error(bot, update, error):
 
 
 def main():
-    updater = Updater("TOKEN HERE")
+    updater = Updater("TOKEN")
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
     updater.start_polling()
+    logger.info("Ready to rock..!")
     updater.idle()
 
 
