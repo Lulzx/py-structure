9189c95689ca44c88a38d3e4dc482fef9e86bf45
Lulzx <wiserishabh@gmail.com>
2019-04-05 22:16:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-import os
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def start(bot, update):
     update.message.reply_text('Hi!')
