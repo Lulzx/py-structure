fc3c031749055d62efc181519b27bad4d6b76b75
Lulzx <wiserishabh@gmail.com>
2019-05-05 13:21:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def start(bot, update):
+def start(update, context):
     update.message.reply_text('Hi!')
 
 
-def help(bot, update):
+def help(update, context):
     update.message.reply_text('Help!')
 
 
-def echo(bot, update):
+def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
-def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"' % (update, error))
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"' % (update, context.error))
 
 
 def main():
