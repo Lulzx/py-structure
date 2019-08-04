348edecad2438e19cb32aa71188a152a3a186aa9
Lulzx <wiserishabh@gmail.com>
2019-08-04 13:57:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,38 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 
-def start(bot, update):
+def start(update, context):
     update.message.reply_text('Hi!')
 
 
-def help(bot, update):
+def help(update, context):
     update.message.reply_text('Help!')
 
 
-def echo(bot, update):
+def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
-def error(bot, update):
-    logger.warning('Update "%s" caused error "%s"' % (update, error))
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
     try:
-        token = sys.argv[1]
+        TOKEN = sys.argv[1]
     except IndexError:
-        token = os.environ.get("TOKEN")
-    updater = Updater(token)
+        TOKEN = os.environ.get("TOKEN")
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
